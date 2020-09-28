3d741bc2f97141c17b63d18f5f71723cd2f0233e
Abby Vander Linden <11965371+avanlinden@users.noreply.github.com>
2020-09-28 15:49:03 -0400

    order to source files in

--- scripts/setup.R
+++ scripts/setup.R
@@ -11,3 +11,27 @@ library(here)
 library(tidyverse)
 
 git_sitrep()
+
+here()
+
+### order of script files:
+
+source(here("scripts/import-stress-data.R"))
+
+source(here("scripts/tidy_stress_data.R"))
+
+source(here("scripts/filter-stress-quantiles.R"))
+
+source(here("scripts/summarize-stress-data.R"))
+
+source(here("scripts/assign-colors.R"))
+
+source(here("scripts/summary-stress-plot.R"))
+
+source(here("scripts/stress-dist-plots.R"))
+
+#source(here("scripts/import-quaternions.R")) #don't need if have already done Python angle conversion
+
+source(here("scripts/tidy-euler-angle-data.R"))
+
+source(here("scripts/rom-dot-plot.R"))
